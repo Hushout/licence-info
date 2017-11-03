1d4103605cb511201e62bc41d42351c4a361cabb
Thibault Odorico <odoricothibault@gmail.com>
2017-11-03 20:04:20 +0100

    Mise à jour

--- S5/HLIN505 - Modélisation et Programmation par Objets 2/TP/TP1/Expression/Constante.java
+++ S5/HLIN505 - Modélisation et Programmation par Objets 2/TP/TP1/Expression/Constante.java
@@ -1,16 +1,16 @@
 public class Constante {
 
-private float valeur;
+	private float valeur;
 
-Constante() {
- this.valeur = 0;	
-}
+	Constante() {
+		this.valeur = 0;	
+	}
 
-Constante(float v){
-this.valeur = v;
-}
+	Constante(float v){
+		this.valeur = v;
+	}
 
-public float eval() {
-	return valeur;
-}
+	public float eval() {
+		return valeur;
+	}
 }
--- S5/HLIN505 - Modélisation et Programmation par Objets 2/TP/TP1/ListeChainВe/TestListeChainee.java
+++ S5/HLIN505 - Modélisation et Programmation par Objets 2/TP/TP1/ListeChainВe/TestListeChainee.java
@@ -2,18 +2,18 @@ import java.io.*;
 
 public class TestListeChainee {
 
-    public static void main(String[] args){
+	public static void main(String[] args){
 
-	ListeChainee list = new ListeChainee();
+		ListeChainee list = new ListeChainee();
 
-	list.addTete(5);
-	list.addTete(4);
-	list.addTete(3);
-	list.addTete(2);
-	list.addTete(1);
+		list.addTete(5);
+		list.addTete(4);
+		list.addTete(3);
+		list.addTete(2);
+		list.addTete(1);
 
-	System.out.println(list);
-	System.out.println(list.reverse());
-	System.out.println("Taille de la liste: " + list.getLength());
-    }
+		System.out.println(list);
+		System.out.println(list.reverse());
+		System.out.println("Taille de la liste: " + list.getLength());
+	}
 }
