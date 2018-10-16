8bb108b394598521047031bf1899454c2698b3bb
Odorico Thibault <thibault.odorico@etu.umontpellier.fr>
2018-10-16 12:55:38 +0200

    Mise à jour du Tue Oct 16 12:55:36 CEST 2018

--- S6/HLIN603 - Objets avances/TPs/2016-2017/TP1/Exercice 1/C.h
+++
@@ -1,8 +0,0 @@
-#include "B.h"
-#include <iostream>
-
-class C : public B{
-public:
-  C(int , int);
-  virtual std::string quiEsTu();
-};
--- S6/HLIN603 - Objets avances/TPs/2016-2017/TP1/Exercice 2/source/PokemonCroisiere.cc
+++
@@ -1,5 +0,0 @@
-#include "../header/PokemonCroisiere.h"
-
-PokemonCroisiere::PokemonCroisiere(std::string s , int n , int m) : PokemonAquatique(s , n , m) {
-  this->vitesse = (float)((this->poids / 25 * this->nbNageoires) / 2);
-}
