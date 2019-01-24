db2d884c15c7555004873858c7ce22f237f40bfa
todorico <odoricothibault@gmail.com>
2019-01-24 12:27:38 +0100

    Mise à jour du jeudi 24 janvier 2019, 12:27:38 (UTC+0100)

--- S6/HLIN611 - Reseaux/TPs/TP2/server.c
+++ S6/HLIN611 - Reseaux/TPs/TP2/server.c
@@ -14,14 +14,14 @@
 
 int main(int argc, char const *argv[])
 {
-	const char* port;
+    const char* port;
 
-	if(argc < 2){
-		fprintf(stderr, "Usage %s <PORT>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+    if(argc < 2){
+        fprintf(stderr, "Usage %s <PORT>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-	port = argv[1];
+    port = argv[1];
 
     struct addrinfo hints, *res;
 
@@ -34,34 +34,34 @@ int main(int argc, char const *argv[])
     printf("Accès à l'adresse liée au port %s...\n", port);
 
     //Pas besoin de spécifier l'hote comme nous créons un serveur.
-	getaddrinfo(NULL, port, &hints, &res);
+    getaddrinfo(NULL, port, &hints, &res);
 
-	printf("Création de la socket...\n");
+    printf("Création de la socket...\n");
 
-	int sock_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	CHECK_FD(sock_fd)
+    int sock_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    CHECK_FD(sock_fd)
 
-	//Permet de réutilliser le port directement après la fin du programme
+    //Permet de réutilliser le port directement après la fin du programme
     //int optval = 1;
-   	//setsockopt(sock_fd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+    //setsockopt(sock_fd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
 
-   	char address[256] = "";
-   	//Convertion de l'adresse IP en chaine de charactère.
-	inet_ntop(AF_INET, &((struct sockaddr_in*)res->ai_addr)->sin_addr, address, sizeof(address));
+    char address[256] = "";
+    //Convertion de l'adresse IP en chaine de charactère.
+    inet_ntop(AF_INET, &((struct sockaddr_in*)res->ai_addr)->sin_addr, address, sizeof(address));
 
-	printf("Affectation de l'adresse %s à la socket...\n", address);
+    printf("Affectation de l'adresse %s à la socket...\n", address);
 
-	int error = bind(sock_fd, res->ai_addr, res->ai_addrlen);
-	CHECK_ERROR(error)
+    int error = bind(sock_fd, res->ai_addr, res->ai_addrlen);
+    CHECK_ERROR(error)
 
     struct sockaddr_storage addr;
     socklen_t addrlen = sizeof(addr);
 
-	printf("Serveur UDP en attente de connexion...\n");
-	printf("IP : %s, PORT : %s\n", address, port);
+    printf("Serveur UDP en attente de connexion...\n");
+    printf("IP : %s, PORT : %s\n", address, port);
 
     while(1){
-    	int n = -1;
+        int n = -1;
         ssize_t byte_count = recvfrom(sock_fd, &n, sizeof(&n), 0, (struct sockaddr*)&addr, &addrlen);
 
         printf("%d\n", n);
@@ -69,5 +69,5 @@ int main(int argc, char const *argv[])
 
     close(sock_fd);
 
-	return 0;
+    return 0;
 }
